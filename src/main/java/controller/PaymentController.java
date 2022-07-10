9252411cba2ef6e236b0bbaaf4b5fb41b8514b4b
khoanguyen77 <nguyentk788@gmail.com>
2022-07-10 19:08:55 +0700

    clean code: InterbankPayloadConverter and AIMSDB

--- src/main/java/controller/PaymentController.java
+++ src/main/java/controller/PaymentController.java
@@ -18,20 +18,23 @@ import subsystem.InterbankSubsystem;
 /**
  * This {@code PaymentController} class control the flow of the payment process
  * in our AIMS Software.
- * 
+ *
  * @author hieud
  *
  */
 
 /*
 	Coincidental cohesion
-	method getExpirationDate is incidental to another method 
+	method getExpirationDate is incidental to another method
 */
 public class PaymentController extends BaseController {
+
 
 	private SimpleCardFactory cardFactory = new SimpleCardFactory();
 	//Vi pham nguyen tac temproral cohession: Viec kiem tra ngay het han cua the se duoc thuc hien truoc khi thanh toan
 	//nhung 2 method nay ko su dung du lieu cua nhau nen co the tach 2 class rieng biet
+
+
 	/**
 	 * Represent the card used for payment
 	 */
@@ -46,7 +49,7 @@ public class PaymentController extends BaseController {
 	 * Validate the input date which should be in the format "mm/yy", and then
 	 * return a {@link String String} representing the date in the
 	 * required format "mmyy" .
-	 * 
+	 *
 	 * @param date - the {@link String String} represents the input date
 	 * @return {@link String String} - date representation of the required
 	 *         format
@@ -80,7 +83,7 @@ public class PaymentController extends BaseController {
 
 	/**
 	 * Pay order, and then return the result with a message.
-	 * 
+	 *
 	 * @param amount         - the amount to pay
 	 * @param contents       - the transaction contents
 	 * @param cardNumber     - the card number
@@ -97,6 +100,7 @@ public class PaymentController extends BaseController {
 		try {
 			expirationDate = getExpirationDate(expirationDate);
 		    //Nen su dung Factory method o day vi co them phuong thuc thanh toan moi 
+
 //			this.card = new CreditCard(
 //					cardNumber,
 //					cardHolderName,
--- src/main/java/subsystem/interbank/InterbankPayloadConverter.java
+++ src/main/java/subsystem/interbank/InterbankPayloadConverter.java
@@ -23,6 +23,19 @@ public class InterbankPayloadConverter {
      * @param contents
      * @return
      */
+
+    /**
+     * Clean code: Chuyển các mã code thành hằng số 
+     */
+    private static final String Success = "00";
+    private static final String InvalidCard = "01";
+    private static final String NotEnoughBalance = "02";
+    private static final String InternalServerError = "03";
+    private static final String SuspiciousTransaction = "04";
+    private static final String NotEnoughTransactionInfo = "05";
+    private static final String InvalidVersion = "06";
+    private static final String InvalidTransactionAmount = "07";
+
     private static InterbankPayloadConverter instance;
     public synchronized static InterbankPayloadConverter getInstance(){
         if (instance == null){
@@ -30,7 +43,7 @@ public class InterbankPayloadConverter {
         }
         return instance;
     }
-    String convertToRequestPayload(CreditCard card, int amount, String contents) {
+    String convertToRequestPayload(Card card, int amount, String contents) {
         Map<String, Object> transaction = new MyMap();
 
         try {
@@ -77,21 +90,21 @@ public class InterbankPayloadConverter {
                 (String) transaction.get("createdAt"));
 
         switch (trans.getErrorCode()) {
-            case "00":
+            case Success:
                 break;
-            case "01":
+            case InvalidCard:
                 throw new InvalidCardException();
-            case "02":
+            case NotEnoughBalance:
                 throw new NotEnoughBalanceException();
-            case "03":
+            case InternalServerError:
                 throw new InternalServerErrorException();
-            case "04":
+            case SuspiciousTransaction:
                 throw new SuspiciousTransactionException();
-            case "05":
+            case NotEnoughTransactionInfo :
                 throw new NotEnoughTransactionInfoException();
-            case "06":
+            case InvalidVersion:
                 throw new InvalidVersionException();
-            case "07":
+            case InvalidTransactionAmount:
                 throw new InvalidTransactionAmountException();
             default:
                 throw new UnrecognizedException();
